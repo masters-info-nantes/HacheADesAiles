e6f69588ed6b2cb431b86588d35b22756627c281
TPichaud <thibaut.pichaud@gmail.com>
2015-11-16 17:43:27 +0100

    Modifiy import

--- src/org/univnantes/alma/hadl/m2/composants/Composant.java
+++ src/org/univnantes/alma/hadl/m2/composants/Composant.java
@@ -3,8 +3,7 @@ package org.univnantes.alma.hadl.m2.composants;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.univnantes.alma.hadl.m2.autres.ContrainteTechnique;
-import org.univnantes.alma.hadl.m2.autres.Propriete;
+import org.univnantes.alma.hadl.m2.autres.*;
 import org.univnantes.alma.hadl.m2.interfaces.Interface;
 
 public abstract class Composant {
